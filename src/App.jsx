65d999057d96d652b57f4126f8ac6417eb5b49e9
Maor Attias <maorattias17@gmail.com>
2024-10-31 16:23:29 -0700

    Projects component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import useWidth from './hooks/useWidth';
 import Desktop from './components/Desktop';
 import Hero from './components/Hero'
 import About from './components/About'
+import Projects from './components/Projects'
 
 
 
@@ -10,9 +11,10 @@ function App() {
   return (
     <> 
       {width > 550 ? <Desktop/> :
-        <div className="bg-gray-900 text-white flex flex-col gap-24 justify-center font-poppins p-5">
+        <div className="font-light bg-gray-900 text-white flex flex-col gap-24 justify-center font-poppins p-5">
           <Hero />
           <About />
+          <Projects />
         </div>
       }
       </>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <>
+      <h2 className="font-bold text-lg">Projects</h2>
+      <a className='no-underline visited:text-white mb-[-70px] mt-[-60px]' href="https://curious-shortbread-fcc9f0.netlify.app/">
+        <div className='mb-2'>Weather App ⬈</div>
+        <img className='rounded-lg' src="../../weather_img.png" alt="" srcset="" />
+      </a>
+      <p>
+      My Weather App is a sleek and user-friendly tool designed to provide accurate, real-time weather updates for any city in the world. Built using React and styled with Tailwind CSS, the app integrates with the OpenWeatherMap API to fetch detailed weather data, including temperature, humidity, wind speed, and a 5-day forecast.
+      </p>
+    </>
+  )
+}
+
+export default Projects
