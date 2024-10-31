8ef369307a795c82fb92138887e3402a16e5fc51
Maor Attias <maorattias17@gmail.com>
2024-10-31 14:26:41 -0700

    Social Media's icons added to Hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import Typewriter from 'typewriter-effect';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons';
 
 function Hero() {
   return (
@@ -7,9 +9,20 @@ function Hero() {
       <div className='ml-5'>
         <h1 className="text-3xl font-bold mb-2">Maor Attias</h1>
         <p className="text-lg mb-2">Frontend Developer</p>
-        <p className="text-gray-400">
+        <p className="text-gray-400 mb-2">
           I build pixel-perfect, engaging, and accessible digital experiences.
         </p>
+        <div className="flex space-x-4 text-white">
+          <a href="https://www.linkedin.com/in/maor-attias-9aa12318a/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="xl" />
+          </a>
+          <a href="https://github.com/maoritz" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="xl" />
+          </a>
+          <a href="https://www.codepen.io" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faCodepen} size="xl" />
+          </a>
+        </div>
       </div>
     </div>
   )
