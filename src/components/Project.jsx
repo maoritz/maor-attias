1a1cbf02def38a1470d92c7c1e8b101ecf1fcd60
Maor Attias <maorattias17@gmail.com>
2024-11-06 18:25:09 -0800

    Media query for 640 breaking point.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,19 +2,25 @@ import LabelButton from "./LabelButton"
 
 function Project({link, name, img, info}) {
   return (
-    <div>
-      <a className='no-underline visited:text-white mb-[-70px] mt-[-60px] mb-2' target="_blank" href={link}>
-        <div className='mb-2 text-teal-300 hover:text-teal-200 duration-300 '>{name} ⬈</div>
-        <img className='rounded-lg mb-3 max-w-sm' src={`../../${img}`} alt="" />
-      </a>
-      <p className="mb-4">
-        {info}
-      </p>
-      <div className="flex gap-1.5">
-        <LabelButton name='React' />
-        <LabelButton name='Tailwind' />
-        <LabelButton name='Github' />
-        <LabelButton name='API' />
+    <div className="text-sm  mb-10 sm:flex flex-row-reverse">
+      <div>
+        <a className='no-underline visited:text-white mb-[-70px] mt-[-60px] mb-2' target="_blank" href={link}>
+          <div className='mb-2 text-teal-300 hover:text-teal-200 duration-300 '>{name} ⬈</div>
+        </a>
+        <p className="mb-4">
+          {info}
+        </p>
+        <div className="flex gap-1.5 mb-3">
+          <LabelButton name='React' />
+          <LabelButton name='Tailwind' />
+          <LabelButton name='Github' />
+          <LabelButton name='API' />
+        </div>
+      </div>
+      <div className="mr-3">
+        <a href={link}>
+          <img className='rounded-lg mb-3 mx-auto' src={`../../${img}`} alt="" />
+        </a>
       </div>
     </div>
   )
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,6 +5,13 @@ const projectsData = [
     name:'Weather App',
     img:'weather_img.png',
     info:' My Weather App is a sleek and user-friendly tool designed to provide accurate, real-time weather updates for any city in the world. Built using React and styled with Tailwind CSS, the app integrates with the OpenWeatherMap API to fetch detailed weather data, including temperature, humidity, wind speed, and a 5-day forecast.'
+  },
+  { 
+    id:1,
+    link:'https://curious-shortbread-fcc9f0.netlify.app/',
+    name:'Weather App',
+    img:'weather_img.png',
+    info:' My Weather App is a sleek and user-friendly tool designed to provide accurate, real-time weather updates for any city in the world. Built using React and styled with Tailwind CSS, the app integrates with the OpenWeatherMap API to fetch detailed weather data, including temperature, humidity, wind speed, and a 5-day forecast.'
   }
 ]
 
